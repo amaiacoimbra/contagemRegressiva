32c448595bd88dcd8b9bab80bf1fd59aa928dccd
Amaia <amaiacoimbra@gmail.com>
2022-04-03 20:54:51 -0300

    att

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ const contagemRegressiva = (tempo) => {
 
 const tempoRestante = () => {
     // 01-01-1970 Data contagem inicio JS
-    const dataEvento = new Date ('2022-05-25 20:50:00')
+    const dataEvento = new Date ('2022-05-29 20:50:00')
     const hoje = Date.now()
     return Math.floor((dataEvento - hoje) / 1000)
 }
